d2a4a6dd3112c7aabb12fcae61e6455e07310b3d
Anil Bosi <bosianils143@gmail.com>
2023-07-18 13:09:19 +0530

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+console.log('JavaScript file is loaded!')
+
+// fetch('http://puzzle.mead.io/puzzle').then(res => res.json()).then(data => console.log(data))
+
+const url = 'http://api.weatherstack.com/current?access_key=8339d98d8a4cdb7ea090ee1c09855ccd&query=37.8267,-122.4233';
+
+
+const inputVal = document.querySelector('input')
+const message1 = document.querySelector('#message1')
+const message2 = document.querySelector('#message2')
+
+
+const weatherForm = document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    message1.textContent = `Temperature : Loading...`
+    message2.textContent = `Weather : Loading...`
+    
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal.value || 'Hyderabad'}&appid=baca9c932141ffc411892ac2ec4c522d&units=metric`;
+    
+    fetch(apiURL).then(res => res.json()).then(data => {
+        console.log(data)
+        message1.textContent = `Temperature : ${data.main.temp}`
+        message2.textContent = `Weather : ${data?.weather[0]?.description}`
+    }).catch(err => {
+        message1.textContent = `Temperature : ${err}`
+        message2.textContent = `Weather : ${err}`
+    })
+    // console.log(inputVal.value);
+
+    // console.log(responseData)
+    console.log('submited')
+})
---
+++ src/app.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+// Define paths for Express config
+const publickDirectoryPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publickDirectoryPath))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        pageDescription: 'Welcome to Home Page.'
+    })
+})
+
+// app.get('/weather', (req, res) => {
+//     if(!req.query.address){
+//         return res.send({
+//             error: "You must provide a address term."
+//         })
+//     }
+
+//     geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+//         if(error){
+//             return res.send({ error })
+//         }
+
+//         forecast(latitude, longitude, (error, forecastData) => {
+//             if(error){
+//                 return res.send({error})
+//             }
+
+//             res.send({
+//                 forecase: forecastData,
+//                 location,
+//                 address: req.query.address
+//             })
+            
+//         })
+//     })
+
+    // res.send({
+    //     address: req.query.address,
+    //     someInfo: 'some info about location',
+    //     climate: 'rainy'
+    // })
+// })
+
+app.get('/products', (req, res) => {
+    console.log(req.query)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page',
+        pageDescription: 'Welcome to About Page.'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        pageDescription: 'This is some helpful text.',
+        title: 'Help Page',
+        name: 'Anil'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404page')
+})
+
+// app.get('/weather', (req, res) => {
+//     res.send([
+//         {
+//             name: 'Anil',
+//             designation: 'MERN Stack Developer'
+//         },
+//         {
+//             name: 'Harish',
+//             designation: 'MERN Stack Developer'
+//         },
+//         {
+//             name: 'Seenu',
+//             designation: 'Software Engineer'
+//         }
+//     ])
+// })
+
+app.listen(3000, () => {
+    console.log('SERVER STARTED..!!')
+})
